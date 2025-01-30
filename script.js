fadfd1c8d4aa231645f9cb08edccf34843832702
l3o9 <getleon09@gmail.com>
2025-01-30 21:45:44 +0600

    added operate function which takes two numbers and an operator then calls required functions on the numbers

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+let result=0;
+
+
 function add(a, b){
     return a+b;
 }
@@ -10,3 +14,25 @@ function multiply(a, b){
 function divide(a, b){
     return (b? Infinity: a/b);
 }
+
+function operate(){
+    let operator=window.prompt('Enter the operator : ');
+    let firstNumber=parseInt(window.prompt('Enter First Number : '));
+    let secondNumber=parseInt(window.prompt('Enter Second Number : '));
+    switch(operator)
+    {
+        case '+':
+            result=add(firstNumber, secondNumber);
+            break;
+        case '-':
+            result=subtract(firstNumber, secondNumber);
+            break;
+        case '*':
+            result=multiply(firstNumber, secondNumber);
+            break;
+        case '/':
+            result=divide(firstNumber, secondNumber);
+            break;
+    }
+}
+operate();
